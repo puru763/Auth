58f0ab532514334d8dbee04b5fd18c4549bfb7e0
Purvesh Krishnani <purvesh152001@gmail.com>
2024-02-06 10:14:47 +0530

    purvesh

---
+++ src/main/java/com/purvesh/Authentication/entity/User.java
@@ -0,0 +1,55 @@
+package com.purvesh.Authentication.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+
+@Data
+@Entity
+@Table(name= "authuser")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Integer id;
+    private String firstname;
+    private String email;
+    private String password;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return  email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
---
+++ src/main/java/com/purvesh/Authentication/services/UserService.java
@@ -0,0 +1,8 @@
+package com.purvesh.Authentication.services;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserService {
+    UserDetailsService userDetailsService();
+
+}
